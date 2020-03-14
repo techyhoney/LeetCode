f71b77c1880cb02995f9d0aaf9cf493db5787d21
techyhoney <61916136+techyhoney@users.noreply.github.com>
2020-03-14 20:02:29 +0530

    Add files via upload

---
+++ Stack_using_2_queue.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+void push();
+void pop();
+void peek();
+int q1[5],f=-1,rear=-1;
+int main()
+{
+int i=0;
+
+        int choice;
+        cout<<("1. Push\n");
+        cout<<("2. Pop\n");
+        cout<<("3. Peek\n\n");
+
+        while(1)
+        {
+            cout<<("Enter Choice: ");
+            cin>>choice;
+
+            switch(choice)
+            {
+                case 1: push();break;
+                case 2: pop();break;
+                case 3: peek();break;
+                default : cout<<("Invalid Choice\n");
+            }
+        }
+    }
+
+    void push()
+    {
+            if(f==rear+1)
+                cout<<("Stack Full\n");
+
+            else if(f==-1 && rear==-1)
+        {
+            cout<<("Enter Element: ");
+            rear=(rear+1)%5;
+            f=(f+1)%5;
+            cin>>q1[rear];
+        }
+            else
+            {
+                cout<<("Enter Element: ");
+                rear=(rear+1)%5;
+                cin>>q1[rear];
+            }
+        }
+
+    void pop()
+    {
+        int i,data;
+        if(f==-1 && rear==-1)
+            cout<<("Stack Empty\n");
+
+        else
+        {
+            for(i=1;i<=rear-1;i++)
+            {
+                data=q1[f];
+                f=(f+1)%5;
+                rear=(rear+1)%5;
+                q1[rear]=data;
+            }
+             data=q1[f];
+              f=(f+1)%5;
+              cout<<"Element Deleted: "<<data<<endl;
+        }
+    }
+
+    void peek(){
+           cout<<q1[rear]<<endl;
+    }
+
+
+
+
