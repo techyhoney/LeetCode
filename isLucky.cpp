537d281b0941670b0654a097278ba491481453d6
techyhoney <61916136+techyhoney@users.noreply.github.com>
2020-04-05 17:02:05 +0530

    Lucky Numbers program using recursion | Microsoft Interview Question
    Lucky Numbers program using recursion | Microsoft Interview Question. 
    Program to check if a number is lucky GeeksforGeeks.

---
+++ isLucky.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+bool isLucky(int n)
+ {
+ static int counter=2;
+  if(counter>n)
+  return 1;
+
+  if(n%counter==0)
+  return 0;
+
+  n=n-n/counter;
+  counter++;
+  return isLucky(n);
+}
+
+int main()
+{
+cout<<boolalpha;
+int n;
+cout<<"Enter a Number: ";
+cin>>n;
+
+cout<<isLucky(n);
+}
